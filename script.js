03be9d76d0fdcb4d396cdfaa01a440285063bae5
soumyabratam6 <144446453+soumyabratam6@users.noreply.github.com>
2025-02-27 20:05:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //your JS code here. If required.
 document.getElementById("submit").addEventListener("click", function () {
     const player1 = document.getElementById("player1").value.trim();
-    const player2 = document.getElementById("player-2").value.trim();
+    const player2 = document.getElementById("player2").value.trim();
 
     if (player1 === "" || player2 === "") {
         alert("Please enter names for both players.");
